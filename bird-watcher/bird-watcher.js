a77a782b317687bbcfd70e561347c8c6c1021d82
yisusworldwide <jesusjimenez.email@gmail.com>
2021-11-11 18:46:46 +0100

    for loops
    Increment/Decrement and for loops concepts.

---
+++ bird-watcher/bird-watcher.js
@@ -0,0 +1,48 @@
+// @ts-check
+//
+// The line above enables type checking for this file. Various IDEs interpret
+// the @ts-check directive. It will give you helpful autocompletion when
+// implementing this exercise.
+
+/**
+ * Calculates the total bird count.
+ *
+ * @param {number[]} birdsPerDay
+ * @returns {number} total bird count
+ */
+export function totalBirdCount(birdsPerDay) {
+    let totalBirds = 0;
+    for (let i = 0; i < birdsPerDay.length; i++) {
+        totalBirds += birdsPerDay[i];
+    }
+    return totalBirds;
+}
+
+/**
+ * Calculates the total number of birds seen in a specific week.
+ *
+ * @param {number[]} birdsPerDay
+ * @param {number} week
+ * @returns {number} birds counted in the given week
+ */
+export function birdsInWeek(birdsPerDay, week) {
+    let totalBirds = 0;
+    for (let i = week * 7 - 7; i < week * 7; i++) {
+        totalBirds += birdsPerDay[i]
+    }
+    return totalBirds
+}
+
+/**
+ * Fixes the counting mistake by increasing the bird count
+ * by one for every second day.
+ *
+ * @param {number[]} birdsPerDay
+ * @returns {number[]} corrected bird count data
+ */
+export function fixBirdCountLog(birdsPerDay) {
+    for (let i = 0; i < birdsPerDay.length - 1; i += 2) {
+        birdsPerDay[i] = birdsPerDay[i] + 1
+    }
+    return birdsPerDay;
+}
